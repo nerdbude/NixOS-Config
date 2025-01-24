378ee753f297265807271a245da24c9c6362c33a
nerdbude <post@nerbude.com>
2025-01-24 10:39:14 +0100

    try to run grafana again

--- server/grafana.nix
+++ server/grafana.nix
@@ -6,17 +6,25 @@
       server = {
         http_addr = "127.0.0.1";
         http_port = 3000;
-        domain = "s.192.168.0.136";
+        domain = "s.kyb3r.space";
         serve_from_sub_path = true;
       };
     };
   };
 
-  services.nginx.virtualHosts."s.192.168.0.136" = {
+  services.nginx.virtualHosts."s.kyb3r.space" = {
+    addSSL = true;
+    enableACME = true;
     locations."/grafana/" = {
       proxyPass = "http://${toString config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port}";
       proxyWebsockets = true;
       recommendedProxySettings = true;
     };
   };
+
+  security.acme.acceptTerms = true;
+  security.acme.certs = {
+    "s.kyb3r.space".email = "post@nerdbude.com";
+  };
+
 }
