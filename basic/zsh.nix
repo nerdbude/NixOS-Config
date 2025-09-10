2d28f4d70d758223707c77eddc145f81d44e7bef
nerdbude <post@nerbude.com>
2025-09-10 11:35:04 +0200

    new zsh config

--- basic/zsh.nix
+++ basic/zsh.nix
@@ -1,12 +1,27 @@
-#----------------------------------------------------
-# MODULE:   zsh.nix
-# FUNCTION: configuration for zsh
-# AUTHOR:   0x17
-# MAIL:     post@nerdbude.com
-# MASTODON: https://corteximplant.com/@0x17
-#-----------------------------------------------------
+{ pkgs, ... }:
+### THEME IMPORT
+### Theme: Passion
+### Dependencies (nixpkgs): 
+### - bc
+let
 
-{ config, pkgs, ... }:
+  geometryTheme = pkgs.fetchFromGitHub {
+    owner = "geometry-zsh";
+    repo = "geometry";
+    rev = "main";
+    sha256 = "FoOY7dkeYC7xQJkX06IDZdduXCfpDxB2aHoSludAMbI=";
+  };
+
+  nzshTheme = pkgs.fetchFromGitHub {
+    owner = "nerdbude";
+    repo = "nzsh";
+    rev = "main";
+    sha256 = "HQI1VUf5Y6MlpM6RXd13PPYSyF6H4Ot4qcodfLpUPck=";
+#    sha256 = "0000000000000000000000000000000000000000000000000000";
+  };
+
+### ZSH CONFIG
+in
 {
 	users.defaultUserShell = pkgs.zsh;
 	programs.zsh = {
@@ -14,17 +29,23 @@
         enableBashCompletion = true;
         enableCompletion = true;
         enableLsColors = true;
+        ohMyZsh = {
+          enable = true;
+          custom = "${nzshTheme}";
+		  theme = "nzsh";
+		  plugins = [ "themes" "colorize" "git" "colored-man-pages" "ssh" ];
+		};
 
         syntaxHighlighting = {
           enable = true;
-          ### DRACULA SYNTAX THEMING
+          ### KYBER.COLOR SSYNTAX THEMING
           styles = {
-            "comment" = "fg=#6272A4";
-            "alias" = "fg=#F1FA8C,bold";
+            "comment" = "fg=#C3C3C3";
+            "alias" = "fg=#FF79C6,bold";
             "suffix-alias" = "fg=#50FA7B";
             "global-alias" = "fg=#50FA7B";
             "function" = "fg=#50FA7B";
-            "command" = "fg=#D6ACFF,italic";
+            "command" = "fg=#FF79C6,bold";
             "precommand" = "fg=#50FA7B,italic";
             "autodirectory" = "fg=#FFB86C,italic";
             "single-hyphen-option" = "fg=#FFB86C";
@@ -34,9 +55,9 @@
             "reserved-word" = "fg=#8BE9FD";
             "hashed-command" = "fg=#8BE9FD";
             "commandseparator" = "fg=#FF79C6";
-            "command-substitution-delimiter" = "fg=#F8F8F2";
-            "command-substitution-delimiter-unquoted" = "fg=#F8F8F2";
-            "process-substitution-delimiter" = "fg=#F8F8F2";
+            "command-substitution-delimiter" = "fg=#C3C3C3";
+            "command-substitution-delimiter-unquoted" = "fg=#C3C3C3";
+            "process-substitution-delimiter" = "fg=#C3C3C3";
             "back-quoted-argument-delimiter" = "fg=#FF79C6";
             "back-double-quoted-argument" = "fg=#FF79C6";
             "back-dollar-quoted-argument" = "fg=#FF79C6";
@@ -47,47 +68,54 @@
             "double-quoted-argument" = "fg=#F1FA8C";
             "double-quoted-argument-unclosed" = "fg=#FF5555";
             "rc-quote" = "fg=#F1FA8C";
-            "dollar-quoted-argument" = "fg=#F8F8F2";
+            "dollar-quoted-argument" = "fg=#C3C3C3";
             "dollar-quoted-argument-unclosed" = "fg=#FF5555";
-            "dollar-double-quoted-argument" = "fg=#F8F8F2";
-            "assign" = "fg=#F8F8F2";
-            "named-fd" = "fg=#F8F8F2";
-            "numeric-fd" = "fg=#F8F8F2";
+            "dollar-double-quoted-argument" = "fg=#C3C3C3";
+            "assign" = "fg=#C3C3C3";
+            "named-fd" = "fg=#C3C3C3";
+            "numeric-fd" = "fg=#C3C3C3";
             "unknown-token" = "fg=#FF5555";
-            "path" = "fg=#FF92DF";
+            "path" = "fg=#FF79C6";
             "path_pathseparator" = "fg=#FF79C6";
-            "path_prefix" = "fg=#FF92DF";
+            "path_prefix" = "fg=#FF79C6";
             "path_prefix_pathseparator" = "fg=#FF79C6";
-            "globbing" = "fg=#F8F8F2";
+            "globbing" = "fg=#C3C3C3";
             "history-expansion" = "fg=#BD93F9";
             "back-quoted-argument-unclosed" = "fg=#FF5555";
-            "redirection" = "fg=#F8F8F2";
-            "arg0" = "fg=#F8F8F2";
-            "default" = "fg=#F8F8F2";
+            "redirection" = "fg=#C3C3C3";
+            "arg0" = "fg=#C3C3C3";
+            "default" = "fg=#C3C3C3";
             "cursor" = "standout";
           };
 
         };
-        ohMyZsh = {
-			enable = true;
-			theme = "refined";
-			plugins = [ "themes" "colorize" "git" "colored-man-pages" "ssh" ];
-		};
 		shellAliases = {
-		    podcast = "podboat -C ~/home/pho/.config/podboat/pod-config";
 			vim = "nvim";
 			ll = "ls -l";
 			update = "sh ./SCRIPTS/rebuilddiff.sh";
 			upgrade = "sh ./SCRIPTS/upgrade.sh";
 			garbage = "sudo nix-collect-garbage";
 			reddit="tuir";
-			wlan="nmtui";
-			matrix="iamb";
-			status="python /home/pho/SCRIPTS/ping.py";
-			email="aerc";
+            wlan="nmtui";
+
+            # LOCAL NODES
+			carter="ssh root@192.168.0.101";
+			finch="ssh root@192.168.0.136";
+			activity="ssh root@159.69.192.195";
+			uconsole="ssh pho@192.168.0.94";
+
+            # EXTERN NODES
+            machine="ssh -i ~/.ssh/matrix root@94.130.224.206";
+			control="ssh root@162.55.61.209";
+			shaw="ssh -p23 u342371@u342371.your-storagebox.de";
+            ingram="ssh root@128.140.64.82";
+            
+
+            matrix="iamb";
+			status="python /home/null/SCRIPTS/ping.py";
+            email="aerc";
 			options="cat ./SCRIPTS/options";
 			accesspoint="sh ./SCRIPTS/accesspoint.sh";
-			uconsole="ssh pho@192.168.0.94";
 		};
 
 	};
